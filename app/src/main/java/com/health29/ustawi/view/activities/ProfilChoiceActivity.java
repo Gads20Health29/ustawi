5d4cbc89a362eafcb0fa4d29e85c1651d8685443
Lionel NKEOUA <mbembasachat9@gmail.com>
2020-10-10 12:14:48 +0100

    initial commit

---
+++ app/src/main/java/com/health29/ustawi/view/activities/ProfilChoiceActivity.java
@@ -0,0 +1,15 @@
+package com.health29.ustawi.view.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import com.health29.ustawi.R;
+
+public class ProfilChoiceActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_profil_choice);
+    }
+}
---
+++ app/src/main/java/com/health29/ustawi/view/activities/SplashScreenActivity.java
@@ -0,0 +1,16 @@
+package com.health29.ustawi.view.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.health29.ustawi.R;
+
+public class SplashScreenActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+    }
+}
